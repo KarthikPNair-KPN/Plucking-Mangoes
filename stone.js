40476b02beebf68f45a392de389d2200904bfbc4
KarthikPNair-KPN <73473583+KarthikPNair-KPN@users.noreply.github.com>
2020-11-25 16:06:54 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -10,7 +10,7 @@ class stone {
         this.y=y
         this.r=r
         this.body=Bodies.circle(this.x,this.y,this.r/2,options)
-        this.image=loadImage("Plucking mangoes/stone.png");
+        this.image=loadImage("stone.png");
 
         World.add(world,this.body);
     }

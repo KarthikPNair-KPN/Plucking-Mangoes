018984a1edb31d96ea35d1ba63f835b85970ca4d
KarthikPNair-KPN <73473583+KarthikPNair-KPN@users.noreply.github.com>
2020-11-25 16:04:56 +0530

    Update boy.js

--- boy.js
+++ boy.js
@@ -8,7 +8,7 @@ class boy {
         this.width=width
         this.height=height 
         this.body=Bodies.rectangle(this.x,this.y,this.width,this.height,options);
-        this.image=loadImage("Plucking mangoes/boy.png");
+        this.image=loadImage("boy.png");
     }
 
     display(){
